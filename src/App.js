f4ca58479f9391988b394c1e469924ef9ca74d0c
est0800 <59091608+est0800@users.noreply.github.com>
2020-07-02 00:25:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,17 @@ import MenuIcon from '@material-ui/icons/Menu'
 import { Container } from '@material-ui/core';
 import Drawer from '@material-ui/core/Drawer'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import BuildIcon from '@material-ui/icons/Build';
+import EmailIcon from '@material-ui/icons/Email';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import FolderIcon from '@material-ui/icons/Folder';
+import HelpIcon from '@material-ui/icons/Help';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SearchIcon from '@material-ui/icons/Search';
 
 function AppBarL(){
     const [open, SetOpen] = useState(false)
@@ -33,12 +44,16 @@ function AppBarL(){
                         Opper
                     </Typography>
                     <div className="log-container">
-                    <Button variant='outlined' color='inherit' style={{marginRight:'5px'}}>
-                        Iniciar sesion
-                    </Button>
-                    <Button variant='outlined' color='inherit'>
-                        Registrarse
-                    </Button>
+                   
+                    <IconButton color='inherit'>
+                        <AccountCircleIcon/>
+                    </IconButton>
+                    <IconButton  color='inherit' style={{marginRight:'5px'}}>
+                        <EmailIcon/>
+                    </IconButton>
+                    <IconButton  color='inherit' style={{marginRight:'5px'}}>
+                        <NotificationsIcon/>
+                    </IconButton>
                     </div>
                 </ToolBar>
             </AppBar>
@@ -49,8 +64,66 @@ function AppBarL(){
             onClose={SetClose}
             >
                 <div className="nav-container">
-                
-                
+                    <li>
+                    <Button>
+                    <BookmarkIcon style={{color:'gray'}}/>
+                    <h5>Publicaciones que te gustaron</h5>
+                    </Button>
+                    </li>
+
+                    <li>
+                    <Button>
+                    <CalendarTodayIcon style={{color:'gray'}} />
+                    <h5>Mis eventos</h5>
+                    </Button>
+                    </li>
+
+                    <li>
+                    <Button>
+                    <BuildIcon style={{color:'gray'}}/>
+                    <h5>Herramientas de trabajo</h5>
+                    </Button>
+                    </li>
+
+                    <li>
+                    <Button>
+                    <ShoppingCartIcon style={{color:'gray'}}/>
+                    <h5>Opper premiun</h5>
+                    </Button>
+                    </li>
+
+                    
+                    
+
+                    <li>
+                    <Button>
+                    <FolderIcon style={{color:'gray'}}/>
+                    <h5>Almacenamiento</h5>
+                    </Button>
+                    </li>
+
+                    <li>
+                    <Button>
+                    <SearchIcon style={{color:'gray'}}/>
+                    <h5>Buscar publicaciones</h5>
+                    </Button>
+                    </li>
+
+                    <li>
+                    <Button>
+                    <SettingsIcon style={{color:'gray'}}/>
+                    <h5>Configuracion</h5>
+                    </Button>
+                    </li>
+
+                    
+                    <li>
+                    <Button>
+                    <HelpIcon style={{color:'gray'}}/>
+                    <h5>Ayuda</h5>
+                    </Button>
+                    </li>
+
                 </div>
             </Drawer>
         </div>
